6eec392c3e172d91c78e9456a41df984bf31f460
Marcos Pernambuco Motta <1091485+mpernambuco@users.noreply.github.com>
2023-08-22 15:37:01 -0300

    feat: Add shadow uarch state

--- pma-defines.h
+++ pma-defines.h
@@ -23,6 +23,10 @@
 #define PMA_SHADOW_PMAS_LENGTH_DEF      0x1000     ///< PMA Array length in bytes
 #define PMA_SHADOW_TLB_START_DEF        0x20000    ///< TLB start address
 #define PMA_SHADOW_TLB_LENGTH_DEF       0x6000     ///< TLB length in bytes
+#define PMA_SHADOW_UARCH_STATE_START_DEF  0x400000 ///< microarchitecture shadow state start address
+#define PMA_SHADOW_UARCH_STATE_LENGTH_DEF 0x1000   ///< microarchitecture shadow state length
+#define PMA_UARCH_RAM_START_DEF         0x600000   ///< microarchitecture RAM start address
+#define PMA_UARCH_RAM_LENGTH_DEF        0x200000   ///< microarchitecture RAM length
 #define PMA_CLINT_START_DEF             0x2000000  ///< CLINT start address
 #define PMA_CLINT_LENGTH_DEF            0xC0000    ///< CLINT length in bytes
 #define PMA_HTIF_START_DEF              0x40008000 ///< HTIF base address (to_host)
@@ -34,7 +38,6 @@
 #define PMA_DHD_LENGTH_DEF              0x1000     ///< Length of in bytes
 #define PMA_PLIC_START_DEF              0x40100000 ///< Start of PLIC range (RESERVED)
 #define PMA_PLIC_LENGTH_DEF             0x00400000 ///< Length of PLIC range (RESERVED)
-#define PMA_UARCH_RAM_START_DEF         0x70000000 ///< microarchitecture RAM start address
 #define PMA_DTB_START_DEF               0x7ff00000 ///< DTB start address
 #define PMA_DTB_LENGTH_DEF              0x100000   ///< DTB length in bytes
 #define PMA_RAM_START_DEF               0x80000000 ///< RAM start address
@@ -57,6 +60,7 @@
 #define PMA_ROLLUP_VOUCHER_HASHES_DID_DEF     10 ///< Device ID for rollup voucher hashes buffer
 #define PMA_ROLLUP_NOTICE_HASHES_DID_DEF      11 ///< Device ID for rollup notice hashes buffer
 #define PMA_DHD_DID_DEF                       12 ///< Device ID for DHD device
+#define PMA_SHADOW_UARCH_STATE_DID_DEF        15 ///< Device ID for uarch shadow state device
 
 // helper for using UINT64_C with defines
 #ifndef EXPAND_UINT64_C
